42bc948d48dc06bc45464bc4be4e772e2e9991b6
jijiranjith <jijiranjithnair@gmail.com>
2024-06-27 11:41:31 +0400

    Add streamlit_app.py for initial deployment

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title('My Streamlit App')
+    st.header('Welcome to Streamlit!')
+    st.write("Here's a simple Streamlit app.")
+
+    # Add more Streamlit components and logic as needed
+
+if __name__ == '__main__':
+    main()
